70d4b10421ec537bba5ed2762701a5de34f20433
amadorcf <amadorcf@outlook.com>
2024-07-16 12:54:25 +0200

    Create Gateway

---
+++ src/main/java/amadorcf/es/YourBank_API_Gateway/YourBankApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package amadorcf.es.YourBank_API_Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+;
+
+@SpringBootApplication
+public class YourBankApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourBankApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/amadorcf/es/YourBank_API_Gateway/YourBankApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package amadorcf.es.YourBank_API_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourBankApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
